51e735cdb0d308c58911432f6ff6b7320f687064
Marin Shalamanov <shalamanov.marin@gmail.com>
2015-08-14 18:20:49 +0300

    fruits are falling

--- app/src/main/java/com/marinshalamanov/kompoti/MainActivity.java
+++ app/src/main/java/com/marinshalamanov/kompoti/MainActivity.java
@@ -1,17 +1,33 @@
 package com.marinshalamanov.kompoti;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     View jar;
+    RelativeLayout layout;
+
+    List<ImageView> fruit;
+
+    final int SPEED_OF_FALLING = 3;
+    int addFruitOnEachMs = 2000;
+
+    long lastFruitAddedAt = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         jar = findViewById(R.id.jar);
+        layout = (RelativeLayout) jar.getParent();
+        fruit = new ArrayList<>();
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        loop();
+                    }
+                }).start(); //.run();
+    }
+
+    void loop() {
+
+        if (lastFruitAddedAt + addFruitOnEachMs < System.currentTimeMillis()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    addFruit();
+                }
+            });
+
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                updateFruit();
+            }
+        });
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            Log.e(getClass().toString(), "error on sleep", e);
+        }
+        loop();
+    }
+
+    void addFruit() {
+
+        ImageView aFruit = new ImageView(this);
+        aFruit.setImageDrawable(getResources().getDrawable(R.drawable.apple));
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(150, 150);
+        params.topMargin = 10;
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int displayWidth = size.x;
+
+        params.leftMargin = new Random().nextInt((int) (displayWidth * 0.8)) + (int) (displayWidth * 0.1);
+
+        layout.addView(aFruit, params);
+        fruit.add(aFruit);
+
+        lastFruitAddedAt = System.currentTimeMillis();
+    }
+
+    void updateFruit() {
+        RelativeLayout.LayoutParams jarParams = (RelativeLayout.LayoutParams) jar.getLayoutParams();
+
+        for (ImageView aFruit : fruit) {
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) aFruit.getLayoutParams();
+            params.topMargin += SPEED_OF_FALLING;
+            aFruit.setLayoutParams(params);
+//
+//            if (params.topMargin > jarParams.topMargin) {
+//                aFruit.setVisibility(View.GONE);
+//            }
+        }
     }
 
     @Override
@@ -27,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         float y = event.getY();
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) jar.getLayoutParams(); //new RelativeLayout.LayoutParams(150, 150);
-        params.leftMargin = (int)x - params.width/2;
+        params.leftMargin = (int) x - params.width / 2;
         jar.setLayoutParams(params);
 
         return super.onTouchEvent(event);
